4688ba7b9da13573a29b6ddb7326b4af27000f4d
Melt628 <Meltryllis628@gmail.com>
2024-04-12 05:21:44 +0800

    path

--- frontend_api/serializer.py
+++ frontend_api/serializer.py
@@ -6,6 +6,8 @@ import json
 import datetime
 import threading
 import os
+import sys 
+sys.path.append("generator")
 from generator.running import run_generation
 class FurnishingRequestSerializer(serializers.ModelSerializer):
 
